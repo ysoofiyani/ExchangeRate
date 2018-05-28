c5809329c2ca417cfea326d297bfa1ae6e8f62e8
Yasser <y.soofiyani@outlook.com>
2018-05-28 19:22:42 -0400

    Initial Commit

---
+++ ExchangeRate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExchangeRate
+//
+//  Created by Yasser on 2018-05-28.
+//  Copyright © 2018 Yasser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
